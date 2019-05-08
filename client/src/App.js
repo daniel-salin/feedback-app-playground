e4c01709c25b8c48eaa76b56e1004d614f2c7a52
Ruben Salas Amner <ruben.salas.amner@chasacademy.se>
2019-05-08 13:59:20 +0200

    add right connect

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import LandingPage from "./components/LandingPage";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.socket = io.connect("https://feedback-socket-backend.herokuapp.com");
+    this.socket = io.connect("https://feedbacktest.herokuapp.com");
   }
 
   getUserHandler = e => {
