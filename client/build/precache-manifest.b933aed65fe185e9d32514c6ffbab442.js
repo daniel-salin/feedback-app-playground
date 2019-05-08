de7a0117071f12a2d177234bcb8745b146dd59c0
Ruben Salas Amner <ruben.salas.amner@chasacademy.se>
2019-05-08 13:50:14 +0200

    Adding build to source control

---
+++ client/build/precache-manifest.b933aed65fe185e9d32514c6ffbab442.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37a9f8df39511f9dcf7e5aba3ab3a30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b70bd7628f10f6cd0b0d",
+    "url": "/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "09f40ccd7662d5271791",
+    "url": "/static/js/2.017bc555.chunk.js"
+  },
+  {
+    "revision": "b70bd7628f10f6cd0b0d",
+    "url": "/static/js/main.5d91cca7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
